426a22504b6968b26db150c628570977b22f61fc
Zilong Huang <843133702@qq.com>
2018-11-28 19:02:52 -0600

    Update run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -6,12 +6,12 @@ date
 
 CS_PATH=$1
 LR=1e-2
-WD=5e-4
-BS=4
+WD=1e-4
+BS=8
 R=2
 INPUT_SIZE=769
 STEPS=60000
-GPU_IDS=0,1
+GPU_IDS=0,1,2,3
 
 #variable ${LOCAL_OUTPUT} dir can save data of you job, after exec it will be upload to hadoop_out path 
 python train.py --data-dir ${CS_PATH} --random-mirror --random-scale --restore-from ./dataset/resnet101-imagenet.pth --gpu ${GPU_IDS} --learning-rate ${LR} --input-size ${INPUT_SIZE},${INPUT_SIZE} --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS} --recurrence ${R}
