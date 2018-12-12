bc3a02364c36568e6a940104914d7fd07222eff9
Zilong Huang <843133702@qq.com>
2018-12-11 22:58:13 -0600

    Update build.sh

--- libs/build.sh
+++ libs/build.sh
@@ -8,5 +8,5 @@ CUDA_GENCODE="\
 
 
 cd src
-nvcc -I/usr/local/cuda-8.0/include --expt-extended-lambda -O3 -c -o bn.o bn.cu -x cu -Xcompiler -fPIC -std=c++11 ${CUDA_GENCODE}
+nvcc -I/usr/local/cuda/include --expt-extended-lambda -O3 -c -o bn.o bn.cu -x cu -Xcompiler -fPIC -std=c++11 ${CUDA_GENCODE}
 cd ..
