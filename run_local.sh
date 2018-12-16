475e7870acc3aafeb35708321d9cb7673ceff80c
Zilong Huang <843133702@qq.com>
2018-12-16 11:29:04 -0600

    Update run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -10,7 +10,7 @@ R=2
 INPUT_SIZE=769
 STEPS=60000
 GPU_IDS=0,1,2,3
-OHEM=0 #set to 1 can reduce the performance gap between val and test set.
+OHEM=0 #set to 1 for reducing the performance gap between val and test set.
 
 #variable ${LOCAL_OUTPUT} dir can save data of you job, after exec it will be upload to hadoop_out path 
 python train.py --data-dir ${CS_PATH} --random-mirror --random-scale --restore-from ./dataset/resnet101-imagenet.pth --gpu ${GPU_IDS} --learning-rate ${LR} --input-size ${INPUT_SIZE},${INPUT_SIZE} --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS} --recurrence ${R} --ohem ${OHEM} --ohem-thres 0.7 --ohem-keep 100000
