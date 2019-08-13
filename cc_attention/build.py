7a76bff58afe5cffab78af921cddda5f10caa611
Zilong Huang <843133702@qq.com>
2019-08-12 20:45:21 -0500

    Update build.py

--- cc_attention/build.py
+++ cc_attention/build.py
@@ -17,7 +17,7 @@ ffi = create_extension(
     relative_to=__file__,
     with_cuda=with_cuda,
     extra_objects=extra_objects,
-    extra_compile_args=["-std=c99"]
+    extra_compile_args=["-std=c++11"]
 )
 
 if __name__ == '__main__':
