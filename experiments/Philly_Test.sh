fde67fffa00c28242bf9c2ae3f9e2c93603ad7ac
stupidZZ <macaroniz1990@gmail.com>
2019-10-23 15:27:57 +0800

    add run_on_philly script, add init.sh

---
+++ experiments/Philly_Test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+uname -a
+date
+
+CS_PATH=/data/home/zhez/data_local/cityscapes/cityscapes.zip@/cityscapes
+RESTORE_FROM=/data/home/zhez/data_local/model/resnet101-imagenet.pth
+SNAPSHOT_HOME=/data/home/zhez/CCNet/snapshot
+LR=1e-2
+WD=5e-4
+BS=4
+STEPS=60000
+OHEM=0
+GPU_IDS=0,1
+
+Judgement=train
+MODEL=baseline
+INPUT_S=769
+INPUT_SIZE=${INPUT_S},${INPUT_S}
+
+if [[ ${Judgement} == 'train' ]]; then
+  CUDA_VISIBLE_DEVICES=${GPU_IDS} python -m torch.distributed.launch --nproc_per_node=2 train.py \
+    --data-dir ${CS_PATH} \
+    --model ${MODEL} \
+    --random-mirror \
+    --random-scale \
+    --restore-from ${RESTORE_FROM} \
+    --input-size ${INPUT_SIZE} \
+    --gpu ${GPU_IDS} \
+    --learning-rate ${LR}  \
+    --weight-decay ${WD} \
+    --batch-size ${BS} \
+    --num-steps ${STEPS} \
+    --ohem ${OHEM} \
+    --snapshot-dir ${SNAPSHOT_HOME}/${MODEL}_${INPUT_S}
+fi
+
+if [[ ${Judgement} == 'evaluate' ]]; then
+  CUDA_VISIBLE_DEVICES=${GPU_IDS} python -m torch.distributed.launch --nproc_per_node=4 evaluate.py \
+    --data-dir ${CS_PATH} \
+    --model ${MODEL} \
+    --input-size ${INPUT_SIZE} \
+    --batch-size 4 \
+    --restore-from ${SNAPSHOT_HOME}/${MODEL}_${INPUT_S}/CS_scenes_${STEPS}.pth \
+    --gpu 0
+fi
---
+++ run_on_philly_pytorch_seg.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+import time
+def ompi_rank():
+    """Find OMPI world rank without calling mpi functions
+    :rtype: int
+    """
+    return int(os.environ.get('OMPI_COMM_WORLD_RANK') or 0)
+
+
+def ompi_size():
+    """Find OMPI world size without calling mpi functions
+    :rtype: int
+    """
+    return int(os.environ.get('OMPI_COMM_WORLD_SIZE') or 1)
+print('rank: {}'.format(ompi_rank()))
+print('env:')
+print(os.environ)
+os.system("ls")
+parser = argparse.ArgumentParser(description='Helper run')
+# general
+parser.add_argument('--auth', help='auth', required=True, type=str)
+parser.add_argument('--cfg', help='experiment configure file name', required=True, type=str)
+parser.add_argument('--branch', help="branch of code", type=str, default='master')
+args, rest = parser.parse_known_args()
+print(args)
+print(rest)
+
+# is_worker = ompi_rank() != 0 and ompi_size() > 1
+os.system("git clone https://{0}@github.com/zdaxie/CCNet -b {1} $HOME/CCNet".format(args.auth, args.branch))
+# only master need to install package
+os.chdir(os.path.expanduser('~/CCNet'))
+os.system('ln -s /hdfs/resrchprojvc4/zhez/data .')
+os.system('ln -s /hdfs/resrchprojvc4/zhez/model .')
+# os.system('ln -s /home/zhez/data_local .')
+os.system('ls')
+os.system('sh init.sh')
+os.chdir(os.path.expanduser('~/CCNet'))
+print('Rank {}'.format(ompi_rank()))
+os.system("sh experiments/{}".format(args.cfg))
